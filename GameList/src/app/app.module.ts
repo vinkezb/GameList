2c1bee86791449a71e2bb7e625dc8e10faaf35c5
vinkezb <kevin.bra99@gmail.com>
2019-03-13 14:01:42 +0100

    menu

--- GameList/src/app/app.module.ts
+++ GameList/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { GameListComponent } from './game-list/game-list.component';
+import { EditGameComponent } from './edit-game/edit-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    GameListComponent,
+    EditGameComponent
   ],
   imports: [
     BrowserModule
